9be3b5b5a6abbb72860077ddd2414ff5e74504ff
Fernando Maymone <fmaymone@gmail.com>
2020-05-29 13:04:27 -0300

    MOD changed API url

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
-import configureStore from '../store'
-import getMenuItems from './menuItems'
-import grants from './grants'
-import locales from './locales'
-import { RMWIcon } from '../components/Icons'
-import { themes } from './themes'
+import configureStore from "../store";
+import getMenuItems from "./menuItems";
+import grants from "./grants";
+import locales from "./locales";
+import { RMWIcon } from "../components/Icons";
+import { themes } from "./themes";
 
 const config = {
   drawer_width: 240,
@@ -14,11 +14,10 @@ const config = {
   themes,
   grants,
   routes: [],
-  baseURL: "https://toptal-backend-fmaymone.c9users.io",
+  baseURL: "https://dry-meadow-93183.herokuapp.com",
   onAuthStateChanged: undefined,
   notificationsReengagingHours: 48,
-  firebaseLoad: () => import('./firebase'),
+  firebaseLoad: () => import("./firebase"),
+};
 
-}
-
-export default config
+export default config;
